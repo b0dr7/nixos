183caca5ebc1aed53cc89bf3d462cc95745aa21c
b0dr7 <b0dr@outlook.com>
2026-04-06 05:07:00 +0000

    fix grub and resolved

--- modules/nixos/services/networking.nix
+++ modules/nixos/services/networking.nix
@@ -1,18 +1,7 @@
 {...}: {
   flake.modules.nixos.networking = {
-    networking = {
-      networkmanager.enable = true;
-      firewall.enable = true;
-     };
-      services.resolved = {
-        enable = true;
-        dnssec = "allow-downgrade";
-        dnsovertls = "yes";
-        domains = ["~."];
-        fallbackDns = [
-          "45.90.28.0#7c81ed.dns.nextdns.io"
-          "45.90.30.0#7c81ed.dns.nextdns.io"
-       ];
+    networking.networkmanager.enable = true;
+    networking.firewall.enable = true;
+    services.resolved.enable = true;
     };
-  };
 }
