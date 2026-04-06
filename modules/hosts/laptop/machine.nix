88ba7b49c0a6285623f4a15c10ee70d08495d106
b0dr7 <b0dr@outlook.com>
2026-04-06 14:50:33 +0000

    fix uuid typo

--- modules/hosts/laptop/machine.nix
+++ modules/hosts/laptop/machine.nix
@@ -149,7 +149,7 @@
     ];
 
     fileSystems."/boot" = {
-      device = "/dev/disk/by-uuid/B0E1-F264";
+      device = "/dev/disk/by-uuid/80E1-F164";
       fsType = "vfat";
       options = ["fmask=0077" "dmask=0077"];
     };
